56daf2bac0c31750ca249dae7f8675293a8900d0
magicien <magicien.du.ballon@gmail.com>
2017-09-17 14:39:04 -0700

    Change NODE_DEV to NODE_ENV

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const logger = require('./logger')
 
 const argv = require('minimist')(process.argv.slice(2))
 const setup = require('./middlewares/frontendMiddleware')
-const isDev = process.env.NODE_DEV !== 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false
 const resolve = require('path').resolve
 const app = express()
@@ -18,31 +18,33 @@ setup(app, {
   publicPath: '/'
 })
 
-if(process.env.NODE_DEV !== 'production'){
+let server = app
+if(isDev){
   const https = require('https')
   const options = {
     key: process.env.SERVER_PRIVATE_KEY,
     cert: process.env.SERVER_CERT
   }
-  const secureServer = https.createServer(options, app).listen(port)
+  server = https.createServer(options, app)
   app.set('port_https', port)
-}else{
-  app.listen(port, host, (err) => {
-    if(err){
-      return logger.error(err.message)
-    }
-  
-    if(ngrok){
-      ngrok.connect(port, (innerErr, url) => {
-        if(innerErr){
-          return logger.error(innerErr)
-        }
-        
-        logger.appStarted(port, prettyHost, url)
-      })
-    }else{
-      logger.appStarted(port, prettyHost)
-    }
-  })
 }
+
+server.listen(port, host, (err) => {
+  if(err){
+    return logger.error(err.message)
+  }
+
+  if(ngrok){
+    ngrok.connect(port, (innerErr, url) => {
+      if(innerErr){
+        return logger.error(innerErr)
+      }
+      
+      logger.appStarted(port, prettyHost, url)
+    })
+  }else{
+    logger.appStarted(port, prettyHost)
+  }
+})
+
 
