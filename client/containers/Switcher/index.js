b42041a6609039facb3ee61f40658fc7e666c990
magicien <magicien.du.ballon@gmail.com>
2017-09-26 00:45:07 -0700

    Fix a Switcher bug

--- client/containers/Switcher/index.js
+++ client/containers/Switcher/index.js
@@ -8,7 +8,8 @@ import universal from 'react-universal-component'
 import { selectPage } from 'redux/modules/location'
 
 const UniversalComponent = universal((props) => import(`pages/${props.page}/index`), {
-  minDelay: 500
+  minDelay: 500,
+  chunkName: (props) => props.page
 })
 
 const Switcher = ({ page }) => (
